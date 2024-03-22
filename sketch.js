509eb058ffc616982856b82df4dbdf94e94eadb0
Ess3013 <eslamessam@aucegypt.edu>
2024-03-22 15:55:07 +0200

    First commit

---
+++ sketch.js
@@ -0,0 +1,69 @@
+//#region main
+function setup() {
+  createCanvas(1000, 112);
+  // background(220);
+
+  escappStart(80);
+
+  let puzzle1 = createPuzzle(20, 550, "The key to escape is to look up and count.");
+  puzzle1.button.mousePressed((func) => solve(1, puzzle1.input.value()));
+
+  let puzzle2 = createPuzzle(20, 650, "The Bishop is praying. Where is he?");
+  puzzle2.button.mousePressed((func) => solve(2, puzzle2.input.value()));
+
+  textAlign(CENTER);
+  textSize(50);
+}
+//#endregion
+
+//#region functions
+function escappStart(roomNumber) {
+  const URI = "https://escapp.es/api/escapeRooms/" + roomNumber + "/start";
+  fetch(URI, {
+    method: "POST",
+    body: JSON.stringify({
+      email: "eslamessam3013@gmail.com",
+      password: "ziCY8AgsX7u3NSh",
+    }),
+    headers: {
+      "Content-type": "application/json",
+      "Accept-Language": "es-ES",
+    },
+  })
+    .then((res) => res.json())
+    .then((res) => console.log(res));
+}
+
+function createPuzzle(x, y, prompt) {
+  let input = createInput();
+  input.position(x, y);
+
+  let button = createButton("submit");
+  button.position(input.x + input.width, y);
+
+  let puzzle = createElement("h3", prompt);
+  puzzle.position(x, y - 2 * input.height);
+
+  return { input: input, button: button, puzzle: puzzle };
+}
+
+function solve(puzzleNum, input) {
+  const solution = input;
+  const URI =
+    "https://escapp.es/api/escapeRooms/80/puzzles/" + puzzleNum + "/submit";
+  fetch(URI, {
+    method: "POST",
+    body: JSON.stringify({
+      email: "eslamessam3013@gmail.com",
+      password: "ziCY8AgsX7u3NSh",
+      solution: solution,
+    }),
+    headers: {
+      "Content-type": "application/json",
+      "Accept-Language": "es-ES",
+    },
+  })
+    .then((res) => res.json())
+    .then((res) => console.log(res));
+}
+//#endregion
